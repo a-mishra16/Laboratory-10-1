63f54d8036827045dc45329cc2e36e93b46998a8
Aanika Mishra <a-mishra@onu.edu>
2024-11-07 09:51:53 -0500

    Initialized Visual Studios Project

---
+++ Laboratory 10-1.cpp
@@ -0,0 +1,11 @@
+/*
+* Project: Laboratory 10-1
+* Author: Aanika Mishra
+* Date: 11-7-2024
+* Description: This program intakes a string statement from the user and counts and returns the number of letters.
+*/
+
+#include <iostream> 
+#include <iomanip>
+
+using namespace std;
